986f78928800da50b8e87755e2a52a28dc47d6b9
Sneha Gupta <71871134+sneha281@users.noreply.github.com>
2022-05-03 13:28:54 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
-import logo from './logo.svg';
+
 import './App.css';
+import Sign from "./components/Sign/sigin"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+    <div className = "App">
+
+    
+    <Sign/>
+  
     </div>
+    </>
   );
 }
 
---
+++ src/components/home/home.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import './home.css';
+const Home = () => {
+  return (
+    <div className ="homepage">
+      <h1>hello homepage me</h1>
+  <div className = "button">button</div>
+    </div>
+  )
+}
+
+export default Home
